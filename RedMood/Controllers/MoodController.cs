7d487dc568626c7f192fb1483caaed8778b2b798
Katarina Peltola <katarina.peltola@redmind.se>
2016-01-04 09:52:15 +0100

    Cleaned up a bit...

--- RedMood/Controllers/MoodController.cs
+++ RedMood/Controllers/MoodController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace RedMood.Controllers
 {
-    public class MoodsController : Controller
+    public class MoodController : Controller
     {
         private RedMoodContext db = new RedMoodContext();
         private MoodService service = new MoodService();
